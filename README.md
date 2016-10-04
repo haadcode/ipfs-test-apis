c10397b6240cddf237bffe4caabeae4c785b257b
haad <haad@headbanggames.com>
2016-10-04 13:57:28 +0200

    First version

---
+++ README.md
@@ -0,0 +1,7 @@
+# ipfs-test-api
+
+Module to help testing orbit-* with multiple IPFS API implementations. 
+
+`js-ipfs-api`, `js-ipfs` and `mem-ipfs`.
+
+**Work in progress**
---
+++ index.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const MemIpfs = require('./mem-ipfs')
+const IPFS = require('ipfs')
+const IPFSDaemon = require('ipfs-daemon')
+
+let ipfsDaemon
+
+const IpfsApis = [
+  {
+    // mem-ipfs
+    name: 'mem-ipfs',
+    start: () => Promise.resolve(MemIpfs),
+    stop: () => Promise.resolve()
+  },
+  {
+    // js-ipfs
+    name: 'js-ipfs',
+    start: () => {
+      return new Promise((resolve, reject) => {
+        const ipfs = new IPFS()
+        // ipfs.goOnline(() => resolve(ipfs))
+        resolve(ipfs)
+      })
+    },
+    stop: () => Promise.resolve()
+    // stop: () => new Promise((resolve, reject) => ipfs.goOffline(resolve))
+  },
+  {
+    // js-ipfs-api via local daemon
+    name: 'js-ipfs-api',
+    start: () => {
+      return IPFSDaemon()
+        .then((res) => {
+          ipfsDaemon = res.daemon
+          return res.ipfs
+        })
+    },
+    stop: () => new Promise((resolve, reject) => ipfsDaemon.stopDaemon(resolve))
+  }
+]
+
+module.exports = IpfsApis
