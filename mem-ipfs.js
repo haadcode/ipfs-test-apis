df471f0df0474b4d3393aebbcd2487b525f8c106
haad <haad@headbanggames.com>
2016-11-25 09:35:10 +0100

    Fix mem-ipfs. beta.2

--- mem-ipfs.js
+++ mem-ipfs.js
@@ -9,9 +9,9 @@ module.exports = {
     get: (hash) => Promise.resolve(objects[hash]),
     put: (data) => {
       return new Promise((resolve, reject) => {
-        return DAGNode.create(new Buffer(data, 'utf-8'), (err, node) => {
+        DAGNode.create(new Buffer(data, 'utf-8'), (err, node) => {
           if (err) reject(err)
-          objects[node.toJSON().hash] = node
+          objects[node.toJSON().multihash] = node
           resolve(node)
         })        
       })
